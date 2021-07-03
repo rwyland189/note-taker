cf8de080f38773bfc9411a207870ccef25e1ac2a
Becca Wyland <rwyland@uwalumni.com>
2021-07-03 11:56:55 -0500

    create get route to display db json data

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require('express');
 // instantiate the server
 const app = express();
 
+// create a route that front-end can request data from
+const { db } = require('./db/db');
+
+// add route to view db json data
+app.get('/api/db', (req, res) => {
+    res.json(db);
+});
+
 // chain listen() method onto our server
 app.listen(3001, () => {
     console.log('API server on port 3001');
